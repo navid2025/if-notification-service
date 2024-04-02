d640a24ec0eb96e50e9d81d54e646203dbfcd3d7
n.zare <n.zare@behsacorp.com>
2024-04-02 16:58:59 +0330

    initial commit

---
+++ src/main/java/com/behsa/notification/IfNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.behsa.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/behsa/notification/IfNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.behsa.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
